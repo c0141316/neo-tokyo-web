7c16990eade9783de7423f1f5e245dcae262dd41
juliocm99 <changmi24@gmail.com>
2021-02-23 00:24:44 +0800

    done with gallery and java script slider

--- behaviour.js
+++ behaviour.js
@@ -9,7 +9,8 @@ var gal = document.getElementById("gallery");
 var fig = document.getElementById("figure");
 var con = document.getElementById("contact");
 var sej = document.getElementById("artSejarah");
-
+var contslid = document.getElementById("artGal");
+var imgslid = ["img1.jpeg", "img2.jpeg", "img3.jpeg", "img4.jpeg"];
 
 function dropDown() {
 if(!dropped) {
@@ -88,4 +89,39 @@ function artSejPopDown(){
 	document.querySelector(".logo").style.top = "auto";
 	document.querySelector(".title").style.fontSize = "8vw"
 	document.querySelector(".subtitle").style.fontSize = "2vw"
+}
+
+function refrSlid() {
+	var i = Math.abs(curslid % 4);
+	contslid.style.backgroundImage = "url(res/slider/" + imgslid[i] + ")";
+}
+
+refrSlid();
+
+function galPopUp() {
+	contslid.style.visibility = "visible";
+	contslid.style.height = "80vh";
+	contslid.style.opacity = "1";
+	gal.style.marginTop = "10vh"
+	wit.style.visibility = "hidden";
+	fig.style.visibility = "hidden";
+	con.style.visibility = "hidden";
+	wit.style.opacity = "0";
+	fig.style.opacity = "0";
+	con.style.opacity = "0";
+	home.style.visibility = "hidden";
+}
+
+function galPopDown() {
+	contslid.style.visibility = "hidden";
+	contslid.style.height = "1vh";
+	contslid.style.opacity = "0";
+	gal.style.marginTop = "26vh"
+	wit.style.visibility = "visible";
+	fig.style.visibility = "visible";
+	con.style.visibility = "visible";
+	wit.style.opacity = "1";
+	fig.style.opacity = "1";
+	con.style.opacity = "1";
+	home.style.visibility = "visible";
 }

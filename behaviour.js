77c49f0048c0806a755696a1b97f8938bd466010
juliocm99 <changmi24@gmail.com>
2021-02-22 18:35:36 +0800

    finished the nav

--- behaviour.js
+++ behaviour.js
@@ -3,130 +3,33 @@ var id2 = null;
 var id3 = null;
 var id4 = null;
 var dropped = false;
-var droppered = false;
+var wit = document.getElementById("whatisthat");
+var gal = document.getElementById("gallery");
+var fig = document.getElementById("figure");
+var con = document.getElementById("contact");
 
 function dropDown() {
 if(!dropped) {
-	let wit = document.getElementById("whatisthat");
-	let gal = document.getElementById("gallery");
-	let fig = document.getElementById("figure");
-	let con = document.getElementById("contact");
-	let mrgntop1 = 10;
-	let mrgntop2 = 10;
-	let mrgntop3 = 10;
-	let mrgntop4 = 10;
-	let opcty = 0;
-	clearInterval(id1);
-	clearInterval(id2);
-	clearInterval(id3);
-	clearInterval(id4);
-	id1 = setInterval(frame1, 30);
-	function frame1() {
-		if (mrgntop1 == 18) {
-		  clearInterval(id1);
-		} else {
-		  mrgntop1++; 
-		  wit.style.marginTop = mrgntop1 + "vh";  
-		}
-		if(opcty == 1) {
-			clearInterval(id1);
-		} else {
-			opcty += 0.1;
-			wit.style.opacity = opcty;
-			gal.style.opacity = opcty;
-			fig.style.opacity = opcty;
-			con.style.opacity = opcty;
-		}
-	}
-	id2 = setInterval(frame2, 30);
-	function frame2() {
-		if (mrgntop2 == 26) {
-			clearInterval(id2);
-		  } else {
-			mrgntop2++; 
-			gal.style.marginTop = mrgntop2 + "vh";  
-		  }
-	}
-	id3 = setInterval(frame3, 30);
-	function frame3() {
-		if (mrgntop3 == 34) {
-			clearInterval(id3);
-		  } else {
-			mrgntop3++; 
-			fig.style.marginTop = mrgntop3 + "vh";  
-		}
-	}
-	id4 = setInterval(frame4, 30);
-	function frame4() {
-		if (mrgntop4 == 42) {
-			clearInterval(id3);
-		  } else {
-			mrgntop4++; 
-			con.style.marginTop = mrgntop4 + "vh";  
-		}
-	}
+	wit.style.marginTop = "18vh";
+	gal.style.marginTop = "26vh";
+	fig.style.marginTop = "34vh";
+	con.style.marginTop = "42vh";
+	wit.style.opacity = 1;
+	gal.style.opacity = 1;
+	fig.style.opacity = 1;
+	con.style.opacity = 1;
 }
 }
 function dropUp() {
-if (!dropped && !droppered) {
-	let wit = document.getElementById("whatisthat");
-	let gal = document.getElementById("gallery");
-	let fig = document.getElementById("figure");
-	let con = document.getElementById("contact");
-	let mrgntop1 = 18;
-	let mrgntop2 = 26;
-	let mrgntop3 = 34;
-	let mrgntop4 = 42;
-	let opcty = 8;
-	clearInterval(id1);
-	clearInterval(id2);
-	clearInterval(id3);
-	clearInterval(id4);
-	id1 = setInterval(frame1, 30);
-	function frame1() {
-		if (mrgntop1 == 10) {
-		  clearInterval(id1);
-		} else {
-		  mrgntop1--; 
-		  wit.style.marginTop = mrgntop1 + "vh";  
-		}
-	}
-	id2 = setInterval(frame2, 30);
-	function frame2() {
-		if (mrgntop2 == 10) {
-			clearInterval(id2);
-		  } else {
-			mrgntop2--; 
-			gal.style.marginTop = mrgntop2 + "vh";  
-		  }
-	}
-	id3 = setInterval(frame3, 30);
-	function frame3() {
-		if (mrgntop3 == 10) {
-			clearInterval(id3);
-		  } else {
-			mrgntop3--; 
-			fig.style.marginTop = mrgntop3 + "vh";  
-		}
-	}
-	id4 = setInterval(frame4, 30);
-	function frame4() {
-		if (mrgntop4 == 10) {
-			clearInterval(id3);
-		  } else {
-			mrgntop4--; 
-			con.style.marginTop = mrgntop4 + "vh";  
-		}
-		if(opcty == 0) {
-			clearInterval(id4);
-		} else {
-			opcty--;
-			wit.style.opacity = opcty/8;
-			gal.style.opacity = opcty/8;
-			fig.style.opacity = opcty/8;
-			con.style.opacity = opcty/8;
-		}
-	}
+if (!dropped) {
+	wit.style.marginTop = "10vh";
+	gal.style.marginTop = "10vh";
+	fig.style.marginTop = "10vh";
+	con.style.marginTop = "10vh";
+	wit.style.opacity = 0;
+	gal.style.opacity = 0;
+	fig.style.opacity = 0;
+	con.style.opacity = 0;
 }
 }
 
